23475efeb5683caeaa92601805a9f88d05e6b7da
Hugo Vallada <valladahugo@gmail.com>
2023-05-20 09:11:35 -0300

    Refactor typealias name

--- src/main/kotlin/com/github/hugovallada/casadocodigo/domain/usecase/ValidateThatEntityDoesNotExistsByUniqueFieldUseCase.kt
+++ src/main/kotlin/com/github/hugovallada/casadocodigo/domain/usecase/ValidateThatEntityDoesNotExistsByUniqueFieldUseCase.kt
@@ -3,12 +3,12 @@ package com.github.hugovallada.casadocodigo.domain.usecase
 import com.github.hugovallada.casadocodigo.domain.gateway.out.GetEntityByUniqueFieldGateway
 import com.github.hugovallada.casadocodigo.shared.validator.isNull
 
-typealias UniqueFieldAndEntityName = Pair<String, String>
+typealias PairOfUniqueFieldAndEntityName = Pair<String, String>
 
 class ValidateThatEntityDoesNotExistsByUniqueFieldUseCase<T>(
     private val getEntityByUniqueFieldGateway: GetEntityByUniqueFieldGateway<T>
 ) {
-    fun execute(data: UniqueFieldAndEntityName) {
+    fun execute(data: PairOfUniqueFieldAndEntityName) {
         check(getEntityByUniqueFieldGateway.execute(data.first).isNull()) {
             "Já existe um ${data.second} com o valor ${data.first}"
         }
